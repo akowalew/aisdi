a39d1eba43e4cd97bb95b77edc344600b8e723c4
akowalew <ram.techen@gmail.com>
2018-11-09 09:35:22 +0100

    Added benchmarks for Vector and List. Reorganized test-CMakeList.txt

---
+++ linear/test/VectorBenchmark.cpp
@@ -0,0 +1,93 @@
+#include <hayai.hpp>
+
+#include <cstdlib>
+ 
+#include <aisdi/Vector.hpp>
+
+constexpr auto Iterations = 100000;
+
+class InsertionBenchmark
+    :   public ::hayai::Fixture
+{
+public:
+	aisdi::Vector<int> container;
+};
+ 
+BENCHMARK_F(InsertionBenchmark, AppendTest, 10, Iterations)
+{
+    container.append(rand() % 10000);
+}
+
+BENCHMARK_F(InsertionBenchmark, PrependTest, 10, Iterations)
+{
+    container.prepend(rand() % 10000);
+}
+
+class ErasingBenchmark
+    :   public ::hayai::Fixture
+{
+public:
+    void SetUp() override
+    {
+    	for(auto i = 0; i < Iterations; ++i)
+    	{
+    		container.append(rand() % 10000);
+    	}
+    }
+
+	aisdi::Vector<int> container;
+};
+ 
+BENCHMARK_F(ErasingBenchmark, PopBackTest, 10, Iterations)
+{
+    container.popBack();
+}
+
+BENCHMARK_F(ErasingBenchmark, PopFrontTest, 10, Iterations)
+{
+    container.popFront();
+}
+
+class CopyingBenchmark
+    :   public ::hayai::Fixture
+{
+public:
+    void SetUp() override
+    {
+    	for(auto i = 0; i < Iterations; ++i)
+    	{
+    		container.append(rand() % 10000);
+    	}
+    }
+
+	aisdi::Vector<int> container;
+};
+
+BENCHMARK_F(CopyingBenchmark, CopyConstructorTest, 100, 1)
+{
+    auto other = container;
+    static_cast<void>(other);
+}
+
+class ComparingBenchmark
+    :   public ::hayai::Fixture
+{
+public:
+    void SetUp() override
+    {
+    	for(auto i = 0; i < Iterations; ++i)
+    	{
+    		container.append(rand() % 10000);
+    		other.append(rand() % 10000);
+    	}
+    }
+
+	aisdi::Vector<int> container;
+	aisdi::Vector<int> other;
+}; 
+
+BENCHMARK_F(ComparingBenchmark, EqualityTest, 1000, 1)
+{
+    const auto equals = (container == other);
+    static_cast<void>(equals);
+}
